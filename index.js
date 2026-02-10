82d49ffe55915e28d7cc12b7b6e3d0fa58ef5568
Salil Thakur <salilthakur09@gmail.com>
2026-02-10 12:52:39 +0530

    redeploy

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ const PORT = process.env.PORT || 3000;
 if (process.env.NODE_ENV !== "production") {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
+    console.log("BFHL API DEPLOYMENT VERSION: v2-validation-fix");
+
   });
 }
 export default app;

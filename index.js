32c5db8caf2502038cc7ab48a3e83e5f0582f0b9
Salil Thakur <salilthakur09@gmail.com>
2026-02-10 12:39:37 +0530

    Added vercel configuration

--- index.js
+++ index.js
@@ -18,8 +18,9 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-  console.log("Gemini Key Loaded:", !!process.env.GEMINI_API_KEY);
-
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+export default app;
